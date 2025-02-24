d220f5a949e706bc9a1da93ed632f24163a5a465
Eliott <eliott.chestnut@gmail.com>
2025-02-24 18:52:42 +0100

    15 :) div and modulo

--- src/ast_multiplicative_expression.cpp
+++ src/ast_multiplicative_expression.cpp
@@ -2,38 +2,64 @@
 
 namespace ast {
 
-    void MultiplicativeExpression::EmitRISC(std::ostream &stream, Context &context, Register destReg) const {
+    void MultiplicativeExpression::EmitRISC(std::ostream &stream, Context &context, Register destReg) const
+    {
         switch (op_) {
             case MultiplicativeOperator::Multiply: {
                 Register leftReg = context.AllocateTemporary();
                 left_->EmitRISC(stream, context, leftReg);
+
                 Register rightReg = context.AllocateTemporary();
                 right_->EmitRISC(stream, context, rightReg);
+
                 stream << "mul " << destReg << "," << leftReg << "," << rightReg << std::endl;
+
                 context.FreeTemporary(leftReg);
                 context.FreeTemporary(rightReg);
                 break;
             }
+
+            case MultiplicativeOperator::Divide: {
+                Register leftReg = context.AllocateTemporary();
+                left_->EmitRISC(stream, context, leftReg);
+
+                Register rightReg = context.AllocateTemporary();
+                right_->EmitRISC(stream, context, rightReg);
+
+                stream << "div " << destReg << "," << leftReg << "," << rightReg << std::endl;
+
+                context.FreeTemporary(leftReg);
+                context.FreeTemporary(rightReg);
+                break;
+            }
+
+            case MultiplicativeOperator::Modulo: {
+                Register leftReg = context.AllocateTemporary();
+                left_->EmitRISC(stream, context, leftReg);
+
+                Register rightReg = context.AllocateTemporary();
+                right_->EmitRISC(stream, context, rightReg);
+
+                // Signed remainder. For unsigned, use 'remu'.
+                stream << "rem " << destReg << "," << leftReg << "," << rightReg << std::endl;
+
+                context.FreeTemporary(leftReg);
+                context.FreeTemporary(rightReg);
+                break;
+            }
+
             case MultiplicativeOperator::UnaryPromote: {
                 right_->EmitRISC(stream, context, destReg);
                 break;
             }
-                // TODO remaining operators
-            case MultiplicativeOperator::Divide: {
-                std::cerr << "Divide not implemented" << std::endl;
-                break;
-            }
-            case MultiplicativeOperator::Modulo: {
-                std::cerr << "Modulo not implemented" << std::endl;
-                break;
-            }
         }
-
     }
 
-    void MultiplicativeExpression::Print(std::ostream &stream) const {
-        if (left_ != nullptr)
+    void MultiplicativeExpression::Print(std::ostream &stream) const
+    {
+        if (left_ != nullptr) {
             left_->Print(stream);
+        }
         switch (op_) {
             case MultiplicativeOperator::Multiply:
                 stream << " * ";
