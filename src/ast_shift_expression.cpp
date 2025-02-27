8035d2eb887f8da7d06a129f95d3a6818c98a3a5
tester <james@xsc.co.uk>
2025-02-27 21:25:05 +0000

    fix left shift

--- src/ast_shift_expression.cpp
+++ src/ast_shift_expression.cpp
@@ -5,7 +5,16 @@ namespace ast {
 
     void ShiftExpression::EmitRISC(std::ostream &stream, Context &context, Register destReg) const {
         switch (op_) {
-            case ShiftOperator::Left:
+            case ShiftOperator::Left: {
+                Register leftReg = context.AllocateTemporary();
+                left_->EmitRISC(stream, context, leftReg);
+                Register rightReg = context.AllocateTemporary();
+                right_->EmitRISC(stream, context, rightReg);
+                stream << "sll " << destReg << "," << leftReg << "," << rightReg << std::endl;
+                context.FreeTemporary(leftReg);
+                context.FreeTemporary(rightReg);
+                break;
+            }
             case ShiftOperator::Right: {
                 Register leftReg = context.AllocateTemporary();
                 left_->EmitRISC(stream, context, leftReg);
