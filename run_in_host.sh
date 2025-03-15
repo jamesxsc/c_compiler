ec6b92552b77e0fad9215c92ce4b11f15e642a19
jamesxsc <james@xsc.co.uk>
2025-03-15 16:22:19 +0000

    3rd time lucky

--- run_in_host.sh
+++ run_in_host.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t compilers_image .
-docker run --rm -it --entrypoint ./test.sh -v "${PWD}:/code" -w "/code" --name "compilers_env" compilers_image
+docker run --rm --entrypoint ./test.sh -v "${PWD}:/code" -w "/code" --name "compilers_env" compilers_image
 
