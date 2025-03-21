34e8e7178b48ed31212b682f8c95b6b37f87e830
jamesxsc <james@xsc.co.uk>
2025-03-21 13:23:42 +0000

    rm old tests

--- compiler_tests/testingnewtests/typedef_enum_pointer_impl.c
+++
@@ -1,6 +0,0 @@
-#include "typedef_enum_pointer.c" 
-static EType globalEnumVal = EVal15;
-EType *extEnumFunc(void)
-{
-    return &globalEnumVal;
-}
--- compiler_tests/testingnewtests/typedef_struct_pointer_impl.c
+++
@@ -1,8 +0,0 @@
-#include "typedef_struct_pointer.c" /* or a matching .h if you have one */
-
-static SType globalStruct = {5, 10};
-
-SType *extStructFunc(void)
-{
-    return &globalStruct;
-}
